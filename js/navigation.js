6132a88fda5d9d57b42a84bebc5b3f2ff5c838e7
HjalmarSnoep <26629952+HjalmarSnoep@users.noreply.github.com>
2019-05-26 15:27:29 +0200

    Add files via upload

---
+++ js/navigation.js
@@ -0,0 +1,11 @@
+(function(){
+	
+	var nav=document.getElementsByTagName("nav")[0];
+	if(nav==null)
+	{
+		window.alert("sorry, couldn't find nav.");
+	}else{
+		window.alert("Found nav");
+	}
+	
+})();
